0daa5e17167e7235c9db560529748764db2b18c3
MishaErr <Mishaerr@gmail.com>
2025-10-22 15:18:48 +0300

    Update map_layers.js

--- js/map_layers.js
+++ js/map_layers.js
@@ -1,6 +1,11 @@
+
 const BASE_LAYERS = {
-  "OSM": L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
-  "Yandex": L.tileLayer('https://core-renderer-tiles.maps.yandex.net/tiles?l=map&x={x}&y={y}&z={z}', {attribution: '© Yandex'}),
-  "Yandex Satellite": L.tileLayer('https://core-sat.maps.yandex.net/tiles?l=sat&x={x}&y={y}&z={z}', {attribution: '© Yandex'}),
+  "OpenStreetMap": L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
+  "Yandex": L.tileLayer('https://core-renderer-tiles.maps.yandex.net/tiles?l=map&x={x}&y={y}&z={z}'),
+  "Yandex Satellite": L.tileLayer('https://core-sat.maps.yandex.net/tiles?l=sat&x={x}&y={y}&z={z}'),
   "ESRI Satellite": L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}')
+  "Landsat": L.tileLayer.wms("https://gibs.earthdata.nasa.gov/wms/epsg3857/best/wms.cgi", {
+  layers: "MODIS_Terra_CorrectedReflectance_TrueColor", format: "image/jpeg", transparent: false
+})
 };
+export { BASE_LAYERS };
